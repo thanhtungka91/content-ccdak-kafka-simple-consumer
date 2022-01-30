dac6d07cb9d0e4f5227d959ddb64a6ec2d4a33b4
Vo Thanh Tung <thanhtungka91@gmail.com>
2022-01-30 22:34:21 +0900

    missing import

--- src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
@@ -1,15 +1,23 @@
 package com.linuxacademy.ccdak.kafkaSimpleConsumer;
 
+import org.apache.kafka.clients.consumer.*;
+import java.util.Properties;
+import java.util.Arrays;
+import java.time.Duration;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        Properties props = new Properties();
+        Properties props = new Properties(); // create a new Properotoes
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "my-group");
-        props.setProperty("enable.auto.commit", "true");
-        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("enable.auto.commit", "true"); //add commit- required
+        props.setProperty("auto.commit.interval.ms", "1000"); // intervew .ms 1s
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        //consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
         consumer.subscribe(Arrays.asList("inventory_purchases"));
         while (true) {
